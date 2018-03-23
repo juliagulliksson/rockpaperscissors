d9fc83729992fe9f44d9f7c38b00bcba17a3a9a4
juliagulliksson <julia.gulliksson@medieinstitutet.se>
2018-03-23 15:35:04 +0100

    Done

--- main.js
+++ main.js
@@ -13,52 +13,11 @@ function confirmWhoWins(){
 }
 
 function registerNumberOfWins(){
-
     const inputValue = inputField.value;
     let isEven = checkIfGameIsEven(inputValue);
    
     if (isEven) {
-        let separatedGames = inputValue.match(/.{1,2}/g);
-        let numberOfTimesBWon = [];
-        let numberOfTimesAWon = [];
-
-        for (let i in separatedGames) {
-
-            switch (separatedGames[i]) {
-                case "RR":
-                case "SS":
-                case "PP":
-                    console.log("Draw");
-                break;
-                case "RS":
-                    console.log("A wins")
-                    numberOfTimesAWon.push(1);
-                break;
-                case "SR":
-                    console.log("B wins");
-                    numberOfTimesBWon.push(1);
-                break;
-                case "SP":
-                    console.log("A wins");
-                    numberOfTimesAWon.push(1);
-                break;
-                case "PS":
-                    console.log("B wins")
-                    numberOfTimesBWon.push(1);
-                break;
-                case "PR":
-                    console.log("A wins")
-                    numberOfTimesAWon.push(1);
-                break;
-                case "RP":
-                    console.log("B wins");
-                    numberOfTimesBWon.push(1);
-                break;
-            }
-        }
-
-        countLength(numberOfTimesAWon, numberOfTimesBWon);
-   
+        determineWhoWinsLoop(inputValue);
     } else {
        displayError();
     }
@@ -69,19 +28,61 @@ function displayError(){
 }
 
 function checkIfGameIsEven(inputValue){
-    if(inputValue.length % 2 == 0){
+    if (inputValue.length % 2 == 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
+function determineWhoWinsLoop(inputValue){
+    let separatedGames = inputValue.match(/.{1,2}/g);
+    let numberOfTimesBWon = [];
+    let numberOfTimesAWon = [];
+
+    for (let i in separatedGames) {
+
+        switch (separatedGames[i]) {
+            case "RR":
+            case "SS":
+            case "PP":
+                console.log("Draw");
+            break;
+            case "RS":
+                console.log("A wins")
+                numberOfTimesAWon.push(1);
+            break;
+            case "SR":
+                console.log("B wins");
+                numberOfTimesBWon.push(1);
+            break;
+            case "SP":
+                console.log("A wins");
+                numberOfTimesAWon.push(1);
+            break;
+            case "PS":
+                console.log("B wins")
+                numberOfTimesBWon.push(1);
+            break;
+            case "PR":
+                console.log("A wins")
+                numberOfTimesAWon.push(1);
+            break;
+            case "RP":
+                console.log("B wins");
+                numberOfTimesBWon.push(1);
+            break;
+        }
+    }
+    countLength(numberOfTimesAWon, numberOfTimesBWon);
+}
+
 function countLength(arrayA, arrayB){
     if (arrayA.length > arrayB.length) {
-        console.log("A wins");
+        console.log("A wins the tournament!");
     } else if(arrayA.length < arrayB.length) {
-        console.log("B wins");
+        console.log("B wins tournament!");
     } else if(arrayA.length == arrayB.length) {
-        console.log("Draw");
+        console.log("The game is a draw");
     }
 }
